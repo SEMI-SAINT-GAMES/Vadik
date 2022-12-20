b1e24d5e879e02d1253e8289ac3275a6117c29e4
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2022-12-20 16:52:45 +0200

    1files

---
+++ Game/Assets/CodeDoorActivator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeDoorActivator : MonoBehaviour
+{
+    public GameObject keypadCanvas;
+    public GameObject doorButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Activate()
+    {
+        keypadCanvas.SetActive(true);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Control control = collision.GetComponent<Control>();
+        if (control != null)
+        {
+            doorButton.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Control control = collision.GetComponent<Control>();
+        if (control != null)
+        {
+            doorButton.SetActive(false);
+        }
+    }
+}
