6fd05aa203f2137ce413b24e058529cdd60b56b0
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2022-12-20 16:55:22 +0200

    2files

---
+++ Game/Assets/BossKir.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKir : MonoBehaviour
+{
+	public bool isFlipped;
+	public Transform player;
+	// Start is called before the first frame update
+	void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void LookAtPlayer()
+	{
+		Vector3 flipped = transform.localScale;
+		flipped.z *= -1f;
+
+		if (transform.position.x > player.position.x && isFlipped)
+		{
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = false;
+		}
+		else if (transform.position.x < player.position.x && !isFlipped)
+		{
+			transform.localScale = flipped;
+			transform.Rotate(0f, 180f, 0f);
+			isFlipped = true;
+		}
+	}
+}
---
+++ Game/Assets/FirstCutSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstCutSceneManager : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player.position.x > 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
