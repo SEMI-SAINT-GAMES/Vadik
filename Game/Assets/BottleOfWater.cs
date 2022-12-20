6ede8f5af37ecf1c7b63b669c2d468850efd2dd5
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2022-12-20 16:59:04 +0200

    6

---
+++ Game/Assets/BottleOfWater.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleOfWater : MonoBehaviour
+{
+    CollectObjectsHero collect;
+    bool isHold;
+    bool colActiv;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collect = GameObject.FindGameObjectWithTag("Player").GetComponent<CollectObjectsHero>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isHold = collect.isHold;
+        if (isHold && !colActiv)
+        {
+           
+            colActiv = true;
+        }
+    }
+   
+}
